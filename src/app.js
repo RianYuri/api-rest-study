b020c25a27f9c732860917f4e16cd9da38d86ace
RianYuri <rianyurirgt@gmail.com>
2023-07-19 09:09:10 -0300

    feat: crud function

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
 const app = express();
-
+app.use(express.json())
 const livros = [
     {id:1, 'titulo':'Senhor dos Aneis'},
-    {id:2, 'titulo':'O Hobit'}
+    {id:2, 'titulo':'O Hobit'},
 ]
 
 app.get('/',(req,res)=>{
@@ -18,4 +18,29 @@ app.get('/livros',(req,res)=>{
 
 })
 
+app.get('/livros/:id',(req,res)=>{
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index])
+    })
+
+app.post('/livros',(req,res)=>{
+    livros.push(req.body);
+    res.status(201).send('Livro cadastrado com sucesso!')
+})
+
+app.put('/livros/:id',(req,res)=>{
+let index = buscaLivro(req.params.id);
+livros[index].titulo = req.body.titulo;
+res.json(livros)
+})
+
+app.delete('/livros/:id',(req,res)=>{
+    let {id} = req.params
+    let index = buscaLivro(id);
+    livros.splice(index,1)
+    res.send(`Livro ${id} removido com sucesso`)
+    })
+const buscaLivro = (id) => {
+return livros.findIndex(livro => livro.id == id)
+}
 export default app;
